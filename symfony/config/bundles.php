7d5f74ab142967413d5d6285be3852bf203b30f8
Adam Busfy <adambusfy2541@gmail.com>
2021-02-18 17:56:04 +0100

    login via Auth0

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -8,5 +8,11 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+//    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true], // add this
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],// add this
 ];
